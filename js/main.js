1843bcbc4b7f5dbdc9df02386abd77f989bc239c
EleanorStrotz <EleanorStrotz@gmail.com>
2015-05-26 11:02:26 -0700

    added working nav bar!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- js/main.js
+++ js/main.js
@@ -43,31 +43,3 @@ parallaxIt();
 
 //nav bar code js
 // Create a clone of the menu, right next to original.
-$('.menu').addClass('original').clone().insertAfter('.menu').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
-
-scrollIntervalID = setInterval(stickIt, 10);
-
-
-function stickIt() {
-
-  var orgElementPos = $('.original').offset();
-  orgElementTop = orgElementPos.top;   
-
-
- // nav bar code
- $(document).ready(function(){
-  $(window).scroll(function(){
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > 49) {
-        $('body').addClass('header-fixed');
-    } else {
-        $('body').removeClass('header-fixed');
-    }
-    // change the style of the navbar when the user scrolls into the next zone.
-    if (scrollTop > 2050) {
-        $('header').addClass('alt-header');
-    } else {
-        $('header').removeClass('alt-header');
-    }
-  });
-});
